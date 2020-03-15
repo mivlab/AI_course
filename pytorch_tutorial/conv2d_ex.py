ec8ed8d36e2c0a22a270ee2e44b03d0295880272
walkup <yl99@163.com>
2020-03-15 11:29:19 +0800

    Added      pytorch_tutorial/conv2d_ex.py Added      pytorch_tutorial/linear_ex.py

---
+++ pytorch_tutorial/conv2d_ex.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import  numpy as np
+import cv2
+
+conv1 = nn.Conv2d(3, 2, 3)
+net = nn.Sequential(conv1)
+conv1.weight.requires_grad_(False)
+conv1.bias.requires_grad_(False)
+# conv1.weight.fill_(0.1)
+# conv1.bias.fill_(0.1)
+
+# RBG数据，格式为chw, 3x4x4
+data = np.array([[[200, 200, 10, 10],
+                   [200, 200, 10, 10],
+                   [200, 200, 10, 10],
+                   [200, 200, 10, 10]],
+                  [[10, 10, 200, 200],
+                   [20, 20, 200, 200],
+                   [10, 10, 200, 200],
+                   [20, 20, 200, 200]],
+                  [[10, 10, 10, 10],
+                   [20, 20, 10, 10],
+                   [10, 10, 10, 10],
+                   [20, 20, 10, 10]]],
+                 dtype=np.float32)
+
+image = torch.tensor(data)
+images = image.unsqueeze(0) # 由chw 变为 nchw， n=1
+out = net(images)
+print(out)
+
+# 保存图像
+data1 = np.swapaxes(data, 0, 2) # 变为whc
+data2 = np.swapaxes(data1, 0, 1) # 变为hwc
+data2 = cv2.cvtColor(data2, cv2.COLOR_RGB2BGR)
+cv2.imwrite('1.jpg', data2)
---
+++ pytorch_tutorial/linear_ex.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import  numpy as np
+import cv2
+
+li = nn.Linear(3, 2)
+net = nn.Sequential(li)
+li.weight.requires_grad_(False)
+li.bias.requires_grad_(False)
+li.weight.fill_(0.1)
+li.bias.fill_(0.1)
+#li.weight[0][0] = 0.5
+#li.weight[0][1] = 0.5
+#li.bias[0] = 1
+
+data = np.array([20, 20, 10], dtype=np.float32)
+data1 = torch.tensor(data)
+out = net(data1)
+print(out)
+
+#for idx, m in enumerate(net.modules()):
+#    print(m)
+#for param in net.parameters():
+#    print(param)
