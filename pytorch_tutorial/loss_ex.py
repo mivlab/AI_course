31361f4c2578c7f6d7bc2ee96a53722a64997e62
walkup <yl99@163.com>
2020-03-23 09:40:12 +0800

    Added      pytorch_tutorial/loss_ex.py Added      pytorch_tutorial/loss_weight_ex.py

---
+++ pytorch_tutorial/loss_ex.py
@@ -0,0 +1,20 @@
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+data = torch.tensor([1.0, -1.0, -1.0]).unsqueeze(0)
+y = torch.tensor([2])
+w=torch.Tensor([1.0, 2.0, 3.0])
+loss_func = nn.CrossEntropyLoss(weight=w)
+loss = loss_func(data, y)
+print(loss)
+
+
+
+
+
+
+
+#import math
+#print(-math.log(math.exp(-1) / (math.exp(1) + math.exp(-1) + math.exp(-1))))
---
+++ pytorch_tutorial/loss_weight_ex.py
@@ -0,0 +1,13 @@
+
+import torch
+import torch.nn as nn
+import  numpy as np
+
+inputs = torch.FloatTensor([0,1,0,0,0,1])
+outputs = torch.LongTensor([0,1])
+inputs = inputs.view((1,3,2))
+outputs = outputs.view((1,2))
+weight_CE = torch.FloatTensor([1,2,3])
+ce = nn.CrossEntropyLoss(ignore_index=255,weight=weight_CE)
+loss = ce(inputs,outputs)
+print(loss)
